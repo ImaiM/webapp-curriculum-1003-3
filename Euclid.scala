b2a7afebc100852376626fcd2d90b23d2312f5e9
ImaiM <onaaksenak@gmail.com>
2018-09-02 17:54:16 +0900

    Edited Euclid.scala to do the process appropriately

--- Euclid.scala
+++ Euclid.scala
@@ -1,7 +1,11 @@
 object Euclid extends App {
 
   def greatestCommonDivisor(a: Int, b: Int): Int = {
-    ???
+      val r = a % b
+    if (r > 0) {
+      greatestCommonDivisor(b, r)
+    } else {
+      return b
   }
-
+}
 }
